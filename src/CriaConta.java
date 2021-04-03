b11f2cc2d52e51e45d60cb3b60b76d1d5c1a5dd2
Evandro S. Motta <evandrosm@live.com>
2021-04-03 03:41:35 -0300

    teste para execício da Alura

---
+++ src/CriaConta.java
@@ -0,0 +1,24 @@
+public class CriaConta {
+    public static void main(String[] args) {
+       Conta primeiraConta = new Conta();
+       primeiraConta.saldo = 200;
+        System.out.println(primeiraConta.saldo);
+
+        primeiraConta.saldo += 100;
+        System.out.println(primeiraConta.saldo);
+
+        Conta segundaConta = new Conta();
+        segundaConta.saldo = 300;
+
+        System.out.println(primeiraConta.agencia);
+        System.out.println(primeiraConta.titular);
+
+        if ( primeiraConta == segundaConta) {
+            System.out.println( "mesma conta");
+        } else {
+            System.out.println("contas diferentes");
+        }
+        System.out.println(primeiraConta);
+        System.out.println(segundaConta);
+    }
+}
